cb88b310956eec40b09d8dce273c858af0cd724c
swapnil_shrungare <swapnil.shrungare123@gmail.com>
2021-07-30 18:33:22 +0530

    Added Logic to calculate moneyness

--- fetch_data.py
+++ fetch_data.py
@@ -15,10 +15,12 @@ data = json.load(f)
 
 nifty = 15778.45
 
-print(type(data))
+df = pd.DataFrame(nifty)
+
+df.head()
 
 #def fetch_oi(expiry_dt):
-#    ce_values = [data['CE'] for data in dajs['records']['data'] if "CE" in data and data['expiryDate'] == expiry_dt]
+#     ce_values = [data['CE'] for data in dajs['records']['data'] if "CE" in data and data['expiryDate'] == expiry_dt]
 ##    pe_values = [data['PE'] for data in dajs['records']['data'] if "PE" in data and data['expiryDate'] == expiry_dt]
 ##
 ##    ce_dt = pd.DataFrame(ce_values).sort_values(['strikePrice'])
--- helper.py
+++ helper.py
@@ -1,15 +1,41 @@
-import requests
 import json
 import pandas as pd
 
+f = open('data.json')
+data = json.load(f)
 
-new_url = 'https://www.nseindia.com/api/marketStatus'
+nifty = 34584.35
 
-headers = {'User-Agent': 'Mozilla/5.0'}
-page = requests.get(new_url,headers=headers)
-dajs = json.loads(page.text)
+df = pd.DataFrame(data)
 
-with open('header.json', 'r') as outfile:
-    print(outfile.readlines())
+def fetch_oi(df,expiry_dt):
+    ce_values = [data['CE'] for data in df['records']['data'] if "CE" in data and data['expiryDate'] == expiry_dt]
+    pe_values = [data['PE'] for data in df['records']['data'] if "PE" in data and data['expiryDate'] == expiry_dt]
 
-  
+    ce_dt = pd.DataFrame(ce_values).sort_values(['strikePrice'])
+    pe_dt = pd.DataFrame(pe_values).sort_values(['strikePrice'])
+    
+    return ce_dt,pe_dt 
+
+exp_date = '05-Aug-2021'
+
+ce_df , pe_df  = fetch_oi(df,exp_date)
+
+#print (ce_df,pe_df)
+
+def moneyness_ce(nifty,strike_price):
+    if strike_price == int(round(nifty, -2)):
+        return  'ATM'
+    elif strike_price > nifty:
+        return 'OTM'
+    elif strike_price < nifty:
+        return 'ITM' 
+    else:
+        return None
+ 
+ce_df['moneyness'] = ce_df.apply(lambda x: moneyness_ce(nifty, x['strikePrice']), axis=1)
+
+ce_df.to_csv('bank.csv')
+dd = ce_df.loc[:,['strikePrice','moneyness']]
+dd.to_csv('bank.csv')
+print(ce_df.loc[:,['strikePrice','moneyness']])
